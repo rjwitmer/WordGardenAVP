f78ebf743575fc6535f15f051f23b5ec149bb70f
Bob Witmer <bwitmer@golden.net>
2025-08-04 09:50:10 -0400

    Initial Commit

---
+++ WordGardenAVP/WordGardenAVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenAVPApp.swift
+//  WordGardenAVP
+//
+//  Created by Bob Witmer on 2025-08-04.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenAVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
